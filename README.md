c54d1581391f277dc5836565ba8fd19ca629f41e
sarahcarric <carricas@oregonstate.edu>
2022-03-04 01:24:13 -0800

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # calendarForCS361
+
+TO RUN
+-pull this repository and then run node server.js and it should work
+
+
+Calendar.handlebars
+-Lines 5-6 change the year and month
+
+-You change the tasks by editing lines 52-53 (55-56 and 60-61 too)
+  -Edit the date between the <p> tags
+  -Edit the title following the class name such as "Do smallsh assignment"
+
+calendarStyle.css
+-the top of the calendarStyle.css (where it says EDIT THIS) is what you'll change to move the tasks.  The comments should explain it pretty well within the top class.
+
+-also use this to change colors 
+
+Let me know if you have questions!!
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static('public'))
 app.get('/', function (req, res, next){
     if (req)
     {
-        res.status(200).render('login')
+        res.status(200).render('calendar')
     }
     else
     {
@@ -44,61 +44,6 @@ app.get('/search', function (req, res, next){
   }
 })
 
-// app.post('/gallery/addPost', function(req, res, next){
-//   var url = req.body.url
-//   var description = req.body.description
-//   var title = req.body.title
-//   var username = req.body.username
-//
-//   if (postData)
-//   {
-//       if (url && description && title && username)
-//       {
-//           postData.push({
-//               url: url,
-//               description: description,
-//               title: title,
-//               username: username
-//           })
-//           fs.writeFile(__dirname + '/postData.json',
-//               JSON.stringify(postData, NULL, 2),
-//               function (err)
-//               {
-//                   if (err)
-//                   {
-//                       res.status(200).send("Data successfully stored.")
-//                   }
-//                   else
-//                   {
-//                       res.status(500).send("Data not received.")
-//                   }
-//               }
-//           )
-//       }
-//       else
-//       {
-//           res.status(400).send("Request body missing input")
-//       }
-//   }
-//   else{
-//     next()
-//   }
-// })
-
-// app.get('/gallery/:post', function(req, res, next){
-//   var post = req.params.post
-//   var postDataArray = postData[post]
-//
-//   if (postDataArray){
-//     res.status(200).render('onePost', {
-//       title: postDataArray.title,
-//       username: postDataArray.username
-//     })
-//   }
-//   else{
-//     next();
-//   }
-// })
 
 app.get('*', function (req, res) {
   res.status(404).render('404', {
